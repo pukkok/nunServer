f1f8c5835dc18d54fd2d2ada541794c9592f3843
Pukkok <krystalgggg@gmail.com>
2024-05-20 18:07:38 +0900

    모든 목록보기 브라우저에서 작업으로 이동

--- src/routes/kinder.js
+++ src/routes/kinder.js
@@ -12,9 +12,14 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0 // 인증서 유효성 검증안함
 const key = config.OPEN_API_KEY
 
 router.post('/kinder', expressAsyncHandler( async(req, res, next) => {
-    const {sidoCode, sggCode, pageCnt, currentPage} = req.body
-    const url = `${BASE_URL}?key=${key}&sidoCode=${sidoCode}&sggCode=${sggCode}`
-    // &currentPage=${currentPage}&pageCnt=${20}
+    const {sidoCode, sggCode} = req.body
+    let url = `${BASE_URL}?key=${key}&sidoCode=${sidoCode}&sggCode=${sggCode}`
+
+    if(Object.keys(req.query).length>0){ // 쿼리스트링이 존재할 때
+        const {page, cnt} = req.query
+        url = `${url}&currentPage=${page}&pageCnt=${cnt}`
+    }
+
     const { data } = await axios.get(url)
     if(data.kinderInfo){
         res.json(data.kinderInfo)
