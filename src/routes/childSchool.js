9dff42aa0d6c61387c94c249259b70783140d6bd
Pukkok <krystalgggg@gmail.com>
2024-06-14 14:33:48 +0900

    원상복귀

--- src/routes/childSchool.js
+++ src/routes/childSchool.js
@@ -8,42 +8,6 @@ const router = express.Router()
 const BASE_URL = config.CHILDSCHOOL_BASE_URL
 const key = config.CHILDSCHOOL_API_KEY
 
-const sggData = require('../datas/sggData')
-
-router.get('/allKinder', expressAsyncHandler( async(req, res, next) => {
-    
-    const success = await Promise.all(
-        sggData.map( item => {
-            const {sidoCode, code : sggCode } = item
-            const url = `${BASE_URL}?key=${key}&sidoCode=${sidoCode}&sggCode=${sggCode}`
-            return axios.get(url)
-        })
-    )
-    if(success){
-        const flat = await success.reduce((acc, r)=>acc.concat(r.data), [])
-        res.json({code: 200, flat})
-    }
-
-}))
-
-async function axiosKinderAllData (allData, setFunc) {
-    const success = await Promise.all(
-        allData.map( item => {
-            const {sidoCode, code} = item
-            return axios.post('/api/kinder', {
-                sidoCode, sggCode : code
-            })
-        })
-    )
-    const flat = await success.reduce((acc, r)=>acc.concat(r.data), [])
-    return setFunc(flat)
-
-}
-
-
-
-
-
 router.post('/kinder', expressAsyncHandler( async(req, res, next) => {
     const {sidoCode, sggCode} = req.body
     const url = `${BASE_URL}?key=${key}&sidoCode=${sidoCode}&sggCode=${sggCode}`
