76fc7c48d1a5779948bfdec8b41de2635933781c
Pukkok <krystalgggg@gmail.com>
2024-06-14 10:49:42 +0900

    인덱스 위치 변경

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const port = 5000
 
 // DB 연결
 const mongoose = require('mongoose')
-const config = require('./config')
-mongoose.connect(config.MONGODB_URL) // 프로미스
+const config = require('../config')
+mongoose.connect(config.MONGODB_URI) // 프로미스
 .then(()=> console.log('데이터베이스 연결 성공'))
 .catch(err => console.log(`데이터베이스 연결 실패 : ${err}`))
 
@@ -13,7 +13,7 @@ mongoose.connect(config.MONGODB_URL) // 프로미스
 
 const cors = require('cors')
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true // 사용자 인증이 필요한 리소스를 요청할 수 있음
 }
 const logger = require('morgan')
@@ -22,22 +22,22 @@ app.use(cors(corsOptions)) // cors 설정
 app.use(logger('tiny')) // 로그 설정
 app.use(express.json()) // 파싱
 app.use(express.static('public'))
-const { isAuth, isAdmin } = require('./auth')
+const { isAuth, isAdmin } = require('../auth')
 /************************************************************************************* */
 
-const childSchool = require('./src/routes/childSchool')
+const childSchool = require('../src/routes/childSchool')
 app.use('/api', childSchool)
 
-const teacher = require('./src/routes/teachers')
+const teacher = require('../src/routes/teachers')
 app.use('/teacher', teacher)
 
-const uploader = require('./src/routes/uploader')
+const uploader = require('../src/routes/uploader')
 app.use('/platform', isAuth, isAdmin, uploader)
 
-const downloader = require('./src/routes/downloader')
+const downloader = require('../src/routes/downloader')
 app.use('/kinder', isAuth, isAdmin, downloader)
 
-const kinder = require('./src/routes/kinder')
+const kinder = require('../src/routes/kinder')
 app.use('/user', kinder)
 
 
--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-    MONGODB_URL : process.env.MONGODB_URL,
+    MONGODB_URI : process.env.MONGODB_URI,
     JWT_SECRET : process.env.JWT_SECRET,
     CHILDSCHOOL_API_KEY : process.env.CHILDSCHOOL_OPENAPI_KEY,
     CHILDSCHOOL_BASE_URL : process.env.CHILDSCHOOL_BASE_URL
